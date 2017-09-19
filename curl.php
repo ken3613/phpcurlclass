9c2271f65a0913f0fc4b6024298ac04ada9307ec
ken3613 <frsy@vip.qq.com>
2017-09-20 00:13:13 +0800

    not complete

--- curl.php
+++ curl.php
@@ -1,15 +1,29 @@
 <?php
+define('REFERER_PC','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36');
 class curl{
 	public $url;
+	public $rCookie;
 	public function __construct($url){
 		$this->url=$url;
 	}
-	public function curlget(){
+	public function curlget($sendCookie,$sReferer){
 		$client=curl_init();
 		curl_setopt($client,CURLOPT_HEADER,0);
 		curl_setopt($client,CURLOPT_HTTPGET,1);
 		curl_setopt($client,CURLOPT_URL,$this->url);
 		curl_setopt($client,CURLOPT_RETURNTRANSFER,1);
+		if(empty($sReferer)){
+			curl_setopt($client,CURLOPT_REFERER,REFERER_PC);
+		}
+		else
+		{
+			curl_setopt($client,CURLOPT_REFERER,$sReferer);
+		}
+		if(!empty($sendCookie)){
+			curl_setopt($client,CURLOPT_COOKIE,$sendCookie);
+		}
+		$this->rCookie=tempnam('./temp','rcookie');
+		curl_setopt($client,CURLOPT_COOKIEJAR,$this->rCookie);
 		$data=curl_exec($client);
 		curl_close($client);
 		return $data;
