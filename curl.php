331cede17eca9fd704da548210a917fb3b9e9612
ken3613 <frsy@vip.qq.com>
2017-09-21 00:10:23 +0800

    set send cookie method for get

--- curl.php
+++ curl.php
@@ -1,26 +1,39 @@
 <?php
-define('REFERER_PC','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36');
+define('UA_PC','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36');
+define('COOKIEMETHOD_NOCOOKIE',0);
+define('COOKIEMETHOD_STRING',1);
+define('COOKIEMETHOD_FILE',2);
 class curl{
 	public $url;
 	public $rCookie;
-	public function __construct($url){
+	public $sCookieMethod;
+	public function __construct($url,$sCookieMethod){
 		$this->url=$url;
+		$this->sCookieMethod=$sCookieMethod;
 	}
-	public function curlget($sendCookie,$sReferer){
+	public function curlget($sendCookie,$sReferer,$sUA){
 		$client=curl_init();
 		curl_setopt($client,CURLOPT_HEADER,0);
 		curl_setopt($client,CURLOPT_HTTPGET,1);
 		curl_setopt($client,CURLOPT_URL,$this->url);
 		curl_setopt($client,CURLOPT_RETURNTRANSFER,1);
-		if(empty($sReferer)){
-			curl_setopt($client,CURLOPT_REFERER,REFERER_PC);
-		}
-		else
-		{
+		if(!empty($sReferer)){
 			curl_setopt($client,CURLOPT_REFERER,$sReferer);
 		}
-		if(!empty($sendCookie)){
-			curl_setopt($client,CURLOPT_COOKIE,$sendCookie);
+		if(!empty($sUA)){
+			curl_setopt($client,CURLOPT_USERAGENT,$sUA);
+		}
+		switch($this->sCookieMethod){
+			case 0:
+				break;
+			case 1:
+				curl_setopt($client,CURLOPT_COOKIE,$sendCookie);
+				break;
+			case 2:
+				curl_setopt($client,CURLOPT_COOKIEFILE,$sendCookie);
+				break;
+			default:
+				break;
 		}
 		$this->rCookie=tempnam('./temp','rcookie');
 		curl_setopt($client,CURLOPT_COOKIEJAR,$this->rCookie);
