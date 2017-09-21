8785361155231878e35affe43d6514e292a580ef
ken3613 <frsy@vip.qq.com>
2017-09-21 22:14:56 +0800

    日常更新

--- curl.php
+++ curl.php
@@ -1,6 +1,6 @@
 <?php
 define('UA_PC','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36');
-define('COOKIEMETHOD_NOCOOKIE',0);
+define('COOKIEMETHOD_NULL',0);
 define('COOKIEMETHOD_STRING',1);
 define('COOKIEMETHOD_FILE',2);
 class curl{
@@ -41,12 +41,32 @@ class curl{
 		curl_close($client);
 		return $data;
 	}
-	public function curlpost($formvalue){
+	public function curlpost($formvalue,$sendCookie,$sReferer,$sUA){
 		$client=curl_init();
 		curl_setopt($client,CURLOPT_POST,1);
 		curl_setopt($client,CURLOPT_URL,$this->url);
 		curl_setopt($client,CURLOPT_RETURNTRANSFER,1);
 		curl_setopt($client,CURLOPT_POSTFIELDS,$formvalue);
+		if(!empty($sReferer)){
+			curl_setopt($client,CURLOPT_REFERER,$sReferer);
+		}
+		if(!empty($sUA)){
+			curl_setopt($client,CURLOPT_USERAGENT,$sUA);
+		}
+		switch($this->sCookieMethod){
+			case 0:
+				break;
+			case 1:
+				curl_setopt($client,CURLOPT_COOKIE,$sendCookie);
+				break;
+			case 2:
+				curl_setopt($client,CURLOPT_COOKIEFILE,$sendCookie);
+				break;
+			default:
+				break;
+		}
+		$this->rCookie=tempnam('./temp','rcookie');
+		curl_setopt($client,CURLOPT_COOKIEJAR,$this->rCookie);
 		$data=curl_exec($client);
 		curl_close($client);
 		return $data;
